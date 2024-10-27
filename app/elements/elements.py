f3cb07d8e8d6eda50e8ebaefaee7f89319423db2
Alex <36799721+MiSTeR1995@users.noreply.github.com>
2024-10-27 22:49:31 +0300

    upd to 0.0.3.2

--- app/elements/elements.py
+++ app/elements/elements.py
@@ -7,18 +7,13 @@ def get_header_html():
             <div class="logo">
                 <img src="https://www.hse.ru/mirror/pubs/share/522215913" alt="Логотип ВШЭ">
                 <div class="logo-title">
-                    Высшая Школа<br>Экономики
+                    <a href="https://hse.ru" target="_blank">Высшая Школа<br>Экономики</a>
                 </div>
             </div>
 
             <!-- Текст -->
             <div class="header-text">
                 Национальный<br>Исследовательский<br>Университет
-            </div>
-
-            <!-- Ссылка на сайт -->
-            <div class="header-link">
-                <a href="https://hse.ru" target="_blank">hse.ru</a>
             </div>
         </div>
     </header>
